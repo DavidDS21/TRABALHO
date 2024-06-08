6e6f24647e3b83868f00c49a4343a4e58d3eff21
DavidDS21 <david.santiago4@aluno.ce.gov.br>
2024-06-08 18:30:34 -0300

    Trabalho Concluído

---
+++ Trabalho/Questão-1.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+print()
+sobrenome = input("Digite seu sobrenome: ")
+print()
+print(f'O seu nome completo é: {nome} {sobrenome}!')
+print()
+print(f"Prazer em conhecê-lo {nome} {sobrenome}")
+print()
+
